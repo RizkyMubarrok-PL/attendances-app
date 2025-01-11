83e5f8db6beae80c0bea39519830345f12ba9302
Rizky Mubarrok <rizki.yamil.com2@gmail.com>
2025-01-11 09:15:15 +0700

    Change some link of file

--- resources/views/template/user/user.blade.php
+++ resources/views/template/user/user.blade.php
@@ -12,7 +12,7 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0-alpha3/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <link rel="stylesheet" href="style/home.css">
+  <link rel="stylesheet" href="{{ asset('style/home.css') }}">
 </head>
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Edu+AU+VIC+WA+NT+Pre:wght@400..700&display=swap');
@@ -81,13 +81,13 @@
     <!-- Sidebar -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4 position-fixed">
       <a href="#" class="brand-link">
-        <img class="brand-image img-circle elevation-2" src="img/logohaloges1.png" alt="">
+        <img class="brand-image img-circle elevation-2" src="{{ asset('img/logohaloges1.png') }}" alt="">
         <span class="font brand-text" style="color: white;">SiHadir</span>
       </a>
       <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="img/anime.png" class="img-circle elevation-2" alt="User Image">
+            <img src="{{ asset('img/anime.png') }}" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
             <a href="#" class="d-block">Guru BK</a>
--- resources/views/template/user/userfooter.blade.php
+++ resources/views/template/user/userfooter.blade.php
@@ -1,8 +1,8 @@
 </div>
 
-<script src="jsnya/jsdua.js"></script>
+<script src="{{ asset('jsnya/jsdua.js') }}"></script>
+<script src="{{ asset('jsnya/js.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
-<script src="jsnya/js.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
