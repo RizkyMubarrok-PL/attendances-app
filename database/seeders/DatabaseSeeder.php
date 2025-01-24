876d6d20d284aa1d208309f3566cf816ae086d52
Rizky Mubarrok <rizki.yamil.com2@gmail.com>
2025-01-24 21:25:53 +0700

    Update database structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,12 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\TestUsers;
+use Database\Seeders\AdminSeeder;
+use Database\Seeders\ClassesSeeder;
+use Database\Seeders\StudentSeeder;
 use Database\Seeders\ClassAndStudents;
+use Database\Seeders\TeacherSeeder;
+use Database\Seeders\TeacherAndClasses;
 use Database\Seeders\StudentAttendances;
 
 class DatabaseSeeder extends Seeder
@@ -17,8 +21,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            TestUsers::class,
+            ClassesSeeder::class,
+            StudentSeeder::class,
             ClassAndStudents::class,
+            TeacherSeeder::class,
+            TeacherAndClasses::class,
             StudentAttendances::class
         ]);
     }
