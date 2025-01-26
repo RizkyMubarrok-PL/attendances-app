fc4503aa3c8159944df440bacdb668e05b81cefb
Bryan C <mahkotahati11@gmail.com>
2025-01-26 11:32:21 +0700

    membenarkan posisi alert, dan responsive

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -18,11 +18,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+
+    </style>
 </head>
 
 <body>
     @if (session('status') === false)
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <div class="alert alert-danger alert-dismissible fade show alert-custom" role="alert">
         <p>{{ session('message') }}</p>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
